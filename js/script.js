43aed75e9d12e0e54375b0f8f8467604cc952878
palina-mrk <palina-mrk@users.noreply.github.com>
2025-08-14 21:51:24 +0000

    deploy: 1471e7b6b08422a2a616a1235e37be62199ab26b

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const initialBGC = getBGC(document.body);
+const inputToRead = document.getElementById("change-bgc__new");
+const buttonToReset = document.getElementById("change-bgc__init");
+
+inputToRead.oninput = () => {
+  setBGC(document.body, event.target.value);
+};
+
+buttonToReset.onclick = () => {
+  setBGC(document.body, initialBGC);
+};
+
+function setBGC(elem, value) {
+  elem.style.backgroundColor = value;
+}
+
+function getBGC(elem) {
+  return elem.style.backgroundColor;
+}
+
+module.exports = {
+  setBGC,
+  getBGC,
+};
