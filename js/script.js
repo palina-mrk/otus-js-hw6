93109095d0a29dfd4300377d69e6030705448921
palina-mrk <khudziakova@tut.by>
2025-08-26 15:00:29 +0300

    weather

--- js/script.js
+++ js/script.js
@@ -1,24 +1,48 @@
-const initialBGC = getBGC(document.body);
-const inputToRead = document.getElementById("change-bgc__new");
-const buttonToReset = document.getElementById("change-bgc__init");
+(async function () {
+  // Получаем указатели на нужные элементы
+  const formEl = document.querySelector("form");
+  const weatherInfoEl = document.querySelector("#weatherInfo");
 
-inputToRead.oninput = () => {
-  setBGC(document.body, event.target.value);
-};
+  function showWeather(el, weatherInfo) {
+    const objectWeather = weatherInfo;
+    const innerHTML = `Погода в городе ${objectWeather.name}:
+состояние: ${objectWeather.weather[0].description},
+температура: ${objectWeather.main.temp} &deg;С,
+ощущается как: ${objectWeather.main.temp} &deg;С,
+ветер: ${objectWeather.wind.speed} м/с`;
+    el.innerHTML = innerHTML;
+    //el.innerHTML = innerHTML + JSON.stringify(weatherInfo, null, 2);
+  }
 
-buttonToReset.onclick = () => {
-  setBGC(document.body, initialBGC);
-};
+  /**
+   * Функция должна делать запрос на
+   * https://api.openweathermap.org/data/2.5/weather?units=metric&q={{CITY_NAME}}&appid={{APP_ID}}
+   * где
+   *  {{CITY_NAME}} должен быть заменен на имя города
+   *  {{APP_ID}} должен быть заменен на ключ приложения
+   * Запрос возвращает данные в формате JSON
+   *
+   * функция должна возвращать (Promise) данные с информацией о погоде
+   * @param {string} cityName
+   */
+  async function getWeather(cityName) {
+    const appId = '63b151efb40928e868a13e6198b120c9';
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${cityName}&units=metric&lang=ru&appid=${appId}`); 
+    const responseJson = await response.json();
+    return responseJson;
+  }
 
-function setBGC(elem, value) {
-  elem.style.backgroundColor = value;
-}
+  formEl.addEventListener("submit", async (ev) => {
+    // чтобы не перезагружать страницу
+    ev.preventDefault();
 
-function getBGC(elem) {
-  return elem.style.backgroundColor;
-}
+    // читаем значение из формы
+    const formElement = ev.target;
+    const inputEl = formElement.querySelector("input");
+    const cityName = inputEl.value;
+    inputEl.value = "";
 
-module.exports = {
-  setBGC,
-  getBGC,
-};
+    const weather = await getWeather(cityName);
+    showWeather(weatherInfoEl, weather);
+  });
+})();
--- js/script.test.js
+++ js/script.test.js
@@ -1,39 +1,39 @@
-/*
- * @jest-environment jsdom
- */
-const fs = require("fs");
-const htmlData = fs.readFileSync("./index.html");
-document.body.innerHTML = htmlData;
+// /*
+//  * @jest-environment jsdom
+//  */
+// const fs = require("fs");
+// const htmlData = fs.readFileSync("./index.html");
+// document.body.innerHTML = htmlData;
 
-const { setBGC, getBGC } = require("./script.js");
+// const { setBGC, getBGC } = require("./script.js");
 
-describe("getBGC(elem) returns background color of the element", () => {
-  it("is a function", () => {
-    expect(getBGC).toBeInstanceOf(Function);
-  });
-  it("correctly returns background color of the element", () => {
-    const newContainer = document.createElement("div");
-    newContainer.style.backgroundColor = "green";
-    document.body.append(newContainer);
+// describe("getBGC(elem) returns background color of the element", () => {
+//   it("is a function", () => {
+//     expect(getBGC).toBeInstanceOf(Function);
+//   });
+//   it("correctly returns background color of the element", () => {
+//     const newContainer = document.createElement("div");
+//     newContainer.style.backgroundColor = "green";
+//     document.body.append(newContainer);
 
-    expect(getBGC(newContainer)).toBe("green");
+//     expect(getBGC(newContainer)).toBe("green");
 
-    newContainer.remove();
-  });
-});
+//     newContainer.remove();
+//   });
+// });
 
-describe("setBGC(elem, color) appends new background color of the element", () => {
-  it("is a function", () => {
-    expect(setBGC).toBeInstanceOf(Function);
-  });
-  it("correctly returns background color of the element", () => {
-    const newContainer = document.createElement("div");
-    newContainer.style.backgroundColor = "green";
-    document.body.append(newContainer);
+// describe("setBGC(elem, color) appends new background color of the element", () => {
+//   it("is a function", () => {
+//     expect(setBGC).toBeInstanceOf(Function);
+//   });
+//   it("correctly returns background color of the element", () => {
+//     const newContainer = document.createElement("div");
+//     newContainer.style.backgroundColor = "green";
+//     document.body.append(newContainer);
 
-    setBGC(newContainer, "red");
-    expect(newContainer.style.backgroundColor).toBe("red");
+//     setBGC(newContainer, "red");
+//     expect(newContainer.style.backgroundColor).toBe("red");
 
-    newContainer.remove();
-  });
-});
+//     newContainer.remove();
+//   });
+// });
