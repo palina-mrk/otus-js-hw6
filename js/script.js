2069a2d0284f58ea1d609f40ded77a7f9006fe61
palina-mrk <khudziakova@tut.by>
2025-08-26 16:10:56 +0300

    add-one-test

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
+function showWeather(el, weatherInfo) {
+  const objectWeather = weatherInfo;
+  const innerHTML = `
+<p>Погода в городе ${objectWeather.name}:</p>
+<p>состояние: ${objectWeather.weather[0].description},</p>
+<p>температура: ${objectWeather.main.temp} &deg;С,</p>
+<p>ощущается как: ${objectWeather.main.temp_feel} &deg;С,</p>
+<p>ветер: ${objectWeather.wind.speed} м/с</p>`;
+  el.innerHTML = innerHTML;
+}
+
+async function getWeather(cityName) {
+  const appId = "63b151efb40928e868a13e6198b120c9";
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${cityName}&units=metric&lang=ru&appid=${appId}`,
+  );
+  const responseJson = await response.json();
+  return responseJson;
+}
+
 async function weather() {
   // Получаем указатели на нужные элементы
   const formEl = document.querySelector("form");
   const weatherInfoEl = document.querySelector("#weatherInfo");
-
-  function showWeather(el, weatherInfo) {
-    const objectWeather = weatherInfo;
-    const innerHTML = `
-<p>Погода в городе ${objectWeather.name}:</p>
-<p>состояние: ${objectWeather.weather[0].description},</p>
-<p>температура: ${objectWeather.main.temp} &deg;С,</p>
-<p>ощущается как: ${objectWeather.main.temp} &deg;С,</p>
-<p>ветер: ${objectWeather.wind.speed} м/с</p>`;
-    el.innerHTML = innerHTML;
-  }
-
-  async function getWeather(cityName) {
-    const appId = "63b151efb40928e868a13e6198b120c9";
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${cityName}&units=metric&lang=ru&appid=${appId}`,
-    );
-    const responseJson = await response.json();
-    return responseJson;
-  }
 
   formEl.addEventListener("submit", async (ev) => {
     // чтобы не перезагружать страницу
@@ -42,4 +42,6 @@ weather();
 
 module.exports = {
   weather,
+  getWeather,
+  showWeather,
 };
--- js/script.test.js
+++ js/script.test.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 const htmlData = fs.readFileSync("index.html");
 document.body.innerHTML = htmlData;
 
-const { weather } = require("./script.js");
+const { weather, showWeather, getWeather } = require("./script.js");
 
 describe("weather", () => {
   it("is a function", () => {
@@ -20,4 +20,43 @@ describe("weather", () => {
 
   //   newContainer.remove();
   // });
+});
+
+describe("getWeather", () => {
+  it("is a function", () => {
+    expect(getWeather).toBeInstanceOf(Function);
+  });
+  // it("correctly returns background color of the element", () => {
+  //   const newContainer = document.createElement("div");
+  //   newContainer.style.backgroundColor = "green";
+  //   document.body.append(newContainer);
+
+  //   expect(getBGC(newContainer)).toBe("green");
+
+  //   newContainer.remove();
+  // });
+});
+
+describe("showWeather", () => {
+  it("is a function", () => {
+    expect(showWeather).toBeInstanceOf(Function);
+  });
+  it("correctly shows the given weather object", () => {
+    const minskWeather = {
+      name: "Минск",
+      weather: [{ description: "ясно" }],
+      main: { temp: 30, temp_feel: 28 },
+      wind: { speed: 18 },
+    };
+    const newContainer = document.createElement("div");
+
+    showWeather(newContainer, minskWeather);
+    document.body.append(newContainer);
+    expect(newContainer.innerHTML.indexOf("Минск") >= 0).toBe(true);
+    expect(newContainer.innerHTML.indexOf("ясно") >= 0).toBe(true);
+    expect(newContainer.innerHTML.indexOf("температура: 30") >= 0).toBe(true);
+    expect(newContainer.innerHTML.indexOf("ощущается как: 28") >= 0).toBe(true);
+    expect(newContainer.innerHTML.indexOf("ветер: 18 м/с") >= 0).toBe(true);
+    newContainer.remove();
+  });
 });
